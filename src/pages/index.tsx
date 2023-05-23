533bf81ed1cf7785dae1f8e861506deeb8289c77
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-05-23 15:58:07 +1000

    social sharing info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,19 @@ export default function Home() {
         <meta name="description" content="Hannah Eich" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Social Sharing */}
+        <meta property="og:title" content="Hannah Eich Portfolio" />
+        <meta
+          property="og:description"
+          content="Fullstack Developer, visit my portfolio page to view my contact info, resume, and projects"
+        />
+        <meta property="og:image" content="/public/images/portolio.png" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="1024" />
+        <meta property="og:url" content="https://hannaheich.com" />
+        <meta property="og:type" content="portfolio website" />
       </Head>
 
       <Hero />
